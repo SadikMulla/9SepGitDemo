3ebbf1e27680ecd49b7201c05accfc5b70c27f97
sadikmulla@gmail.com <sadikmulla2274@gmail.com>
2024-01-29 15:05:28 +0530

    Added basic infrastructure of framework

---
+++ src/Pages/loginPage.java
@@ -0,0 +1,15 @@
+package Pages;
+
+public class loginPage {
+	
+	public void loginApp()
+	{
+		System.out.println("Login Page");
+	}
+	
+	public void featchApp()
+	{
+		System.out.println("Featch Page");
+	}
+
+}
